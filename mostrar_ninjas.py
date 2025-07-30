89d0599f362e6803065fa3519e7397915e8d6eed
emely711 <maite.bone02@gmail.com>
2025-07-29 23:06:27 -0500

    Update mostrar_ninjas.py

--- mostrar_ninjas.py
+++ mostrar_ninjas.py
@@ -1,1 +1,6 @@
-
+def mostrar_ninjas(ninjas):
+    if not ninjas:
+        print("No hay ninjas registrados.")
+        return
+    for n in ninjas:
+        print(f"ID: {n['id']} - Nombre: {n['nombre']} - Fuerza: {n['fuerza']} - Agilidad: {n['agilidad']} - Resistencia: {n['resistencia']} - Habilidad: {n['habilidad']}")
